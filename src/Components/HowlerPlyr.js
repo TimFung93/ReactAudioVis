9c7853c1e8b546bc85b0dbf342b645679a6b94dc
Jermain Joseph <jermjoseph@gmail.com>
2017-05-20 20:45:40 -0400

    Working Howler audio player

--- src/Components/HowlerPlyr.js
+++ src/Components/HowlerPlyr.js
@@ -11,8 +11,9 @@ export default class Player  extends Component {
             
         }
 
-    this.handleOnPlay = this.handleOnPlay.bind(this)
-    this.handleStop = this.handleStop.bind(this)
+    this.handleOnPlay= this.handleOnPlay.bind(this)
+    this.handleStop= this.handleStop.bind(this)
+    this.buildAnalyzer= this.buildAnalyzer.bind(this) 
     }
 
     // start music
@@ -29,21 +30,50 @@ export default class Player  extends Component {
             playing: false
         })
     }
-    // pause music
+
+    //Build web audio analyzer
+
+    buildAnalyzer(){
+        
+    }
+
     
     render() {
+
         return (
             <div>
                 <ReactHowler
                     src='http://goldfirestudios.com/proj/howlerjs/sound.ogg'
-                    playing={false}
+                    playing={
+                        this.state.playing
+                    }
+                    volume={this.state.volume}
+
                 />
-                <button onClick={this.handleOnPlay}>
+                <button onClick= {this.handleOnPlay}>
                     Play 
                 </button>   
-                <button onClick={this.handleStop}>
+                <button onClick= {this.handleStop}>
                     Stop
-                 </button>      
+                 </button>
+                 <div className= 'volume'>
+                    <label> 
+                        Volume:
+                        <span className= 'slider-container'>
+                            <input 
+                                type= 'range' 
+                                min= '0'
+                                max= '1'
+                                step= '0.05'
+                                onChange= {e=> this.setState({volume: parseFloat(e.target.value)})}
+                                value="{this.state.volume}"
+                                style= {{verticalAlign: 'bottom'}}
+                            />
+                        </span>
+                        {this.state.volume.toFixed(2)}
+                    </label>
+                 </div>
+                 
             </div>
             )
 
